b983ae9686586f06e87da40e10ba189128bc02ea
Hristo Deshev <hristo@deshev.com>
2019-10-25 13:17:26 +0300

    Move py3status and config to Nix

--- i3/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ln -s ~/.config/dotfiles/i3 ~/.config/i3
-~/.config/i3/install-py3status.sh
--- i3/py3status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$(dirname $(readlink -f "$0"))
-VENV=$SCRIPT_DIR/venv
-
-exec $VENV/bin/py3status --interval 2 -c $SCRIPT_DIR/status-bar.config
